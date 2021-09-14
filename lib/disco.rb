ae5aba1da23908eba0d4c29ed5c0e27cc92ab195
c_tcountz <c_tcountz@groupon.com>
2021-09-14 09:22:40 -0400

    add comments

--- lib/disco.rb
+++ lib/disco.rb
@@ -9,11 +9,13 @@ class Disco
   def self.write(raw_event)
     event = JSON.parse(raw_event)
 
-    existing_columns = Dir.children(DATASTORE_DIR)
     index_filepath = File.join(DATASTORE_DIR, INDEX_FILE)
     head = File.open(index_filepath, 'r') { |index| index.gets.to_i }
+
+    existing_columns = Dir.children(DATASTORE_DIR)
     new_columns = (event.keys - existing_columns)
 
+    # Backfill `nil` (`\n`) for each new column file based on current index
     new_columns.each do |column|
       filepath = File.join(DATASTORE_DIR, column)
       File.open(filepath, 'a') do |file|
@@ -21,6 +23,8 @@ class Disco
       end
     end
 
+    # Each column file will have the event value appended to it
+    # If the column doesn't appear in the event, `event[column]` returns nil
     Dir.each_child(DATASTORE_DIR) do |column|
       filepath = File.join(DATASTORE_DIR, column)
       File.open(filepath, 'a') do |file|
@@ -28,6 +32,7 @@ class Disco
       end
     end
 
+    # Update the index
     File.open(index_filepath, 'w') do |index|
       index.puts head.next
     end
